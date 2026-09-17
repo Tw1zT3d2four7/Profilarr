4abe132d950bd3cd821b858e4afe5df2061050b1
Jeremy Isenhour <144488925+Tw1zT3d2four7@users.noreply.github.com>
2026-09-17 14:22:03 -0400

    Refactor ffmpeg command in profilarr.sh
    Refactor ffmpeg command for better readability and add stream mapping.

--- profilarr.sh
+++ profilarr.sh
@@ -1,2 +1,31 @@
 #!/bin/sh
-ffmpeg -user_agent "$1" -reconnect 1 -reconnect_at_eof 1 -reconnect_streamed 1 -reconnect_delay_max 5 -multiple_requests 1 -seekable 0 -fflags +genpts+igndts -analyzeduration 5M -probesize 5M -i "$2" -c:v copy -c:a aac -b:a 128k -ac 2 -async 1 -mpegts_copyts 0 -muxdelay 0 -muxpreload 0 -avoid_negative_ts make_zero -mpegts_flags +resend_headers+pat_pmt_at_frames+initial_discontinuity -f mpegts pipe:1 2>/dev/null | cvlc --no-video-title-show --network-caching=6000 --sout="#std{access=file,mux=ts,dst=-}" fd://0
+ffmpeg \
+-user_agent "$1" \
+-reconnect 1 \
+-reconnect_at_eof 1 \
+-reconnect_streamed 1 \
+-reconnect_delay_max 5 \
+-multiple_requests 1 \
+-seekable 0 \
+-fflags +genpts+igndts+discardcorrupt \
+-analyzeduration 5M \
+-probesize 5M \
+-i "$2" \
+-map 0:v:0 \
+-map 0:a:0 \
+-c:v copy \
+-c:a aac \
+-b:a 128k \
+-ac 2 \
+-async 1 \
+-mpegts_copyts 0 \
+-avoid_negative_ts make_zero \
+-muxdelay 0 \
+-muxpreload 0 \
+-mpegts_flags +resend_headers+pat_pmt_at_frames+initial_discontinuity \
+-f mpegts pipe:1 2>/dev/null | \
+cvlc \
+--no-video-title-show \
+--network-caching=6000 \
+--sout="#std{access=file,mux=ts,dst=-}" \
+fd://0
